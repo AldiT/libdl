b80b071c4325768cd1e878d511c8abf26ed7fce0
Aldi T <aldi_topalli@yahoo.com>
2019-05-05 15:42:06 +0200

    Restructured

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <functional>
 
-#include "TutorialConfig.h"
-
 int main()
 {
     [out = std::ref(std::cout << "Hello")](){out.get() << " World!\n";}();
 
-    TutorialConfig t;
-    t.test_func();
 }
 
