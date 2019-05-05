f85fff8f910f6698474b359469c524e389b9e371
Aldi T <aldi_topalli@yahoo.com>
2019-05-06 01:19:48 +0200

    Libraries paths

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
+//#define CATCH_CONFIG_MAIN
 #include <iostream>
 #include <functional>
 
-int main()
-{
-    [out = std::ref(std::cout << "Hello")](){out.get() << " World!\n";}();
+#include "extern/Catch.hpp"
+#include "test.cpp"
 
+#include <Eigen/Dense>
+#include <pybind11.h>
+
+using namespace Eigen;
+namespace py = pybind11;
+
+int main(int argc, char **argv)
+{
+    Matrix3d m =  Matrix3d::Random();
+    m = (m + Matrix3d::Constant(1.2)) * 50;
+
+    Vector3d v(1, 2, 3);
+
+    std::cout << "m * v" << std::endl << m * v << std::endl;
+
+    return 0;
 }
 
