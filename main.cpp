be9d3b1effe1d60c9a12d24d3a7a203d7c5d2294
Aldi T <aldi_topalli@yahoo.com>
2019-05-07 12:16:40 +0200

    Temporary deletion

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
-//#define CATCH_CONFIG_MAIN
 #include <iostream>
 #include <functional>
 
-#include "extern/Catch.hpp"
-
-#include <Eigen/Dense>
-#include <pybind11.h>
-
-using namespace Eigen;
-namespace py = pybind11;
 
 int main(int argc, char **argv)
 {
-    Matrix3d m =  Matrix3d::Random();
-    m = (m + Matrix3d::Constant(1.2)) * 50;
-
-    Vector3d v(1, 2, 3);
-
-    std::cout << "m * v" << std::endl << m * v << std::endl;
 
     return 0;
 }
